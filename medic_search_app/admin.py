4ca7fc0157aa89682d36e1a6d2de8c7f459c7a0c
Lucas Oliveira <lucasio2008@gmail.com>
2024-01-25 13:18:58 -0300

    feat(medic_search/medic_search_app): customizable.
    customizable profile template in admin.

--- medic_search_app/admin.py
+++ medic_search_app/admin.py
@@ -1,7 +1,37 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    date_hierarchy = 'crated_at'
+    list_display = ('user', 'role', 'birth',)
+    list_display_links = ('user', 'role',)
+    list_filter = ('user__is_active',)
+    search_fields = ('user__username',)
+    fieldsets = (
+        ('Usuário', {
+        'fields': ('user', 'birthday', 'image',)
+        }),
+        ('Função', {
+        'fields': ('role',)
+        }),
+        ('Extras', {
+        'fields': ('specialties', 'addresses',)
+        }),
+    )
+
+    class Media:
+        css = {
+            'all': ('css/custom.css',)
+        }
+        js = ('js/custom.js',)
+
+    def birth(self, obj):
+        if obj.birthday:
+            return obj.birthday.strftime('%d/%m/%Y')
+        
+    birth.empty_value_display = '___/___/_____'
+
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(State)
 admin.site.register(City)
 admin.site.register(Neighborhood)
